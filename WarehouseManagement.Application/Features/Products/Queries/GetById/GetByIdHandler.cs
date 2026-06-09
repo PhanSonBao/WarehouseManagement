bdd6ab2af78d9de2f3a849712a62f41363e5ed86
Admin <psonbao@gmail.com>
2026-06-09 11:00:09 +0700

    Add product queries

--- WarehouseManagement.Application/Features/Products/Queries/GetById/GetByIdHandler.cs
+++ WarehouseManagement.Application/Features/Products/Queries/GetById/GetByIdHandler.cs
@@ -1,7 +1,5 @@
-using AutoMapper;
 using MediatR;
 using WareHouseManagement.Application.Features.Products.Queries.GetProductById;
-using WarehouseManagement.Domain.Entities;
 using WarehouseManagement.Domain.Interfaces;
 
 namespace WarehouseManagement.Application.Features.Products.Queries.GetById;
@@ -9,34 +7,35 @@ namespace WarehouseManagement.Application.Features.Products.Queries.GetById;
 public class GetByIdHandler : IRequestHandler<GetByIdQuery, ProductDto>
 {
     // 1. Tạo private readonly field cho IProductRepository
-    private readonly IProductRepository productRepository;
+    private readonly IProductRepository _productRepository;
     
     // 2. Nhận IProductRepository trong constructor
-    public GetByIdHandler()
+    public GetByIdHandler(IProductRepository productRepository)
     {
-        this.productRepository = productRepository;
+        _productRepository = productRepository;
     }
     
     public async Task<ProductDto> Handle(GetByIdQuery query, CancellationToken cancellationToken)
     {
-        // Map Product entity sang ProductDto
-        // Gợi ý: return new ProductDto(product.Id, product.SKU, ...)
-
-        // Return dto
-
-        var product = await productRepository.GetByIdAsync(query.Id, cancellationToken);
+        var product = await _productRepository.GetByIdAsync(query.Id, cancellationToken);
         if (product == null)
         {
             throw new KeyNotFoundException();
         }
-
-        Mapper.CreateMap<Product, ProductDto>();
-
-        return new ProductDto(
-        {
-            product.Id
-        });
         
-        return ProductDto;
+        // Return dto
+        return new ProductDto
+        {
+            Id = product.Id,
+            Sku = product.Sku,
+            Name =  product.Name,
+            Description = product.Description,
+            CostPrice =  product.CostPrice,
+            SalePrice = product.SalePrice,
+            Barcode = product.BarCode,
+            CategoryId =  product.CategoryId,
+            BrandId =  product.BrandId,
+            IsActive =  product.IsActive,
+        };
     }
 }
--- WarehouseManagement.Application/Features/Products/Queries/GetById/ProductDto.cs
+++ WarehouseManagement.Application/Features/Products/Queries/GetById/ProductDto.cs
@@ -2,14 +2,14 @@ namespace WarehouseManagement.Application.Features.Products.Queries.GetById;
 
 public record ProductDto
 {
-    Guid ProductId;
-    string Sku;
-    string Name;
-    string Description;
-    decimal CostPrice;
-    decimal SalePrice;
-    string? Barcode;
-    Guid? CategoryId;
-    Guid? BrandId;
-    bool IsActive;
+    public Guid Id { get; set; }
+    public string Sku { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal CostPrice { get; set; }
+    public decimal SalePrice { get; set; }
+    public string? Barcode { get; set; }
+    public Guid? CategoryId { get; set; }
+    public Guid? BrandId { get; set; }
+    public bool IsActive { get; set; }
 }
